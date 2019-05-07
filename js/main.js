6e5e0b28d4849c445c43f9e26cda14e58047993e
jordan birkeland <jbirkeland@mcad.edu>
2019-05-06 19:37:52 -0500

    Added places array and data for each object

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var vm = new Vue ({
+    el: "#places",
+    data: {
+        places: [
+            {
+                placeName: "Bergen, Norway",
+                tripDescription: "History and Hiking in the Homeland",
+                tripDuration: "10 days",
+                tripMonth: "August and September",
+                tripCost: "$5,000",
+                lodgingAndFoodIncl: "Yes",
+                accommodationType: "4 nights in hotels, 4 nights camping",
+                airfareIncl: "No",
+                activityLevel: "Moderate, some hiking involved",
+                weather: "Rainy"
+            },
+            {
+                placeName: "San Diego, California",
+                tripDescription: "A Sunny Start to Spring in Southern California",
+                tripDuration: "4 days",
+                tripMonth: "March and April",
+                tripCost: "$1,000",
+                lodgingAndFoodIncl: "Yes",
+                accommodationType: "3 nights in hotels",
+                airfareIncl: "Yes",
+                activityLevel: "Moderate, several long walks per day",
+                weather: "Sunny"
+            },
+            {
+                placeName: "Tofte, Minnesota",
+                tripDescription: "Nothing but Nature on the North Shore",
+                tripDuration: "5 days",
+                tripMonth: "June and July",
+                tripCost: "$300",
+                lodgingAndFoodIncl: "Yes",
+                accommodationType: "4 nights camping",
+                airfareIncl: "No",
+                activityLevel: "Difficult, several long hikes per day",
+                weather: "Partly Sunny, Occasional Rain"
+            },
+            {
+                placeName: "Mykonos, Greece",
+                tripDescription: "Relaxing in the Magnificent Mediterranean",
+                tripDuration: "10 days",
+                tripMonth: "September and October",
+                tripCost: "$7,000",
+                lodgingAndFoodIncl: "Yes",
+                accommodationType: "8 nights in hotels",
+                airfareIncl: "No",
+                activityLevel: "Easy, beaches and great food",
+                weather: "Partly Sunny, Windy"
+            },
+
+        ]
+    }
+})
